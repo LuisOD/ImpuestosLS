f0127db310a1ba35bd2e88bbe1c98f511f8388a6
LuisOD <mediaplusxv@gmail.com>
2017-01-20 07:16:25 -0600

    31

--- src/Excepciones/IntervalosfechaException.java
+++ src/Excepciones/IntervalosfechaException.java
@@ -7,19 +7,9 @@ package Excepciones;
  */
 public class IntervalosfechaException extends Exception{
     
-    /**
-     * Creates a new instance of <code>IntervalosFechaException</code> without
-     * detail message.
-     */
     public IntervalosfechaException() {
     }
 
-    /**
-     * Constructs an instance of <code>IntervalosFechaException</code> with the
-     * specified detail message.
-     *
-     * @param msg the detail message.
-     */
     public IntervalosfechaException(String msg) {
         super(msg);
     }
